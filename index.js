8d4e82cef6fc731bfbf14e39be01855e0aabcd00
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2019-11-21 22:19:14 -0700

    added dishIdRouter Node Module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishIdRouter = require('./routes/dishIdRouter');
 const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
@@ -11,9 +12,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-
-
 app.use('/dishes', dishRouter);
+app.use('/dishes/dishId', dishIdRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
